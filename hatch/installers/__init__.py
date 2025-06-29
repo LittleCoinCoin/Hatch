1c575d06afcd8c603fca5992cb38b21ebd39306b
LittleCoinCoin <eliott.jacopin@laposte.net>
2025-06-29 17:54:32 +0900

    [Fix] Package import & discovery
    **Major**!
    - Updating the toml to discover the packages automatically (setuptool)
    - Aded the imports to all installer such that the installer registry will be populated immediately upon import

--- hatch/installers/__init__.py
+++ hatch/installers/__init__.py
@@ -5,13 +5,21 @@ implementations for different dependency types including Hatch packages,
 Python packages, system packages, and Docker containers.
 """
 
-from .installer_base import DependencyInstaller, InstallationError, InstallationContext
-from .registry import InstallerRegistry, installer_registry
+from hatch.installers.installer_base import DependencyInstaller, InstallationError, InstallationContext
+from hatch.installers.hatch_installer import HatchInstaller
+from hatch.installers.python_installer import PythonInstaller
+from hatch.installers.system_installer import SystemInstaller
+from hatch.installers.docker_installer import DockerInstaller
+from hatch.installers.registry import InstallerRegistry, installer_registry
 
 __all__ = [
     "DependencyInstaller",
     "InstallationError", 
     "InstallationContext",
+    #"HatchInstaller", # Not necessary to expose directly, the registry will handle it
+    #"PythonInstaller", # Not necessary to expose directly, the registry will handle it
+    #"SystemInstaller", # Not necessary to expose directly, the registry will handle it
+    #"DockerInstaller", # Not necessary to expose directly, the registry will handle it
     "InstallerRegistry",
     "installer_registry"
 ]
